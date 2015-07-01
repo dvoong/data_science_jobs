17bbc81e27e4068b30776301803cbd7055e5f65a
David Voong <voong.david@gmail.com>
2015-07-01 21:23:43 +0100

    Added a basic view for the home page and wrote some tests for it

--- data_science_jobs/urls.py
+++ data_science_jobs/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'data_science_jobs.views.home_page'),
     url(r'^admin/', include(admin.site.urls)),
 ]
