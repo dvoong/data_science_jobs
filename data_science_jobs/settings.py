162881fdb458024e38bb694ed3cbade55a1a2062
David Voong <voong.david@gmail.com>
2015-07-01 18:39:53 +0100

    Moved the database outside of the repository

--- data_science_jobs/settings.py
+++ data_science_jobs/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'data_science_jobs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.abspath(os.path.join(BASE_DIR, '../database/db.sqlite3')),
     }
 }
 
